a6708625ee275b21d71a419df4af25630569338e
Davis <davis@Daviss-MBP.home>
2020-07-20 11:37:50 +0100

    Refactoring

--- Join/src/com/davis/course/MyThread.java
+++ Join/src/com/davis/course/MyThread.java
@@ -4,9 +4,11 @@ public class MyThread extends Thread {
 
 	@Override
 	public void run() {
+		
 		try {
 			Thread.sleep(1500);
 		} catch (InterruptedException e) {e.printStackTrace();}
+		
 		System.out.println("Exiting thread...");
 	}
 }
