627108fe0c6620f9bf77e702b805051324d23864
Davis <davis@Daviss-MBP.home>
2020-07-24 10:34:59 +0100

    Refactoring

--- ExplicitLock/src/com/davis/course/App.java
+++ ExplicitLock/src/com/davis/course/App.java
@@ -6,7 +6,7 @@ import java.util.concurrent.locks.ReentrantLock;
 public class App {
 
 	private final Lock lock = new ReentrantLock();
-	private int count = 0;
+	private int count;
 	
 	public static void main(String[] args) throws InterruptedException {
 		new App().increment();
@@ -17,8 +17,7 @@ public class App {
 		lock.lock();
 	    
 		try {
-	        count++;
-	        System.out.println("I'm the only thread that now can increase the counter to " + count);
+	        System.out.println("I'm the only thread that now can increase the counter to " + ++count);
 	    } finally {
 	        lock.unlock();
 	        System.out.println("Now I released the lock");
